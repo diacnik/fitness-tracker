a9c7977076676d8d024dfaaaa7aa0f40db548326
Ian Kincaid <i.s.kincaid@gmail.com>
2026-03-10 19:43:35 -0400

    added NavBar with placeholder labels

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const isActive = ref(false);
+
+</script>
+
+<template>
+  <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+  <div class="container">
+    <div class="navbar-brand">
+    <RouterLink to="/" class="navbar-item has-text-weight-semibold">
+      Fitness Tracker
+    </RouterLink>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
+      @click="isActive = !isActive" :class="{ 'is-active': isActive }">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isActive}">
+    <div class="navbar-start">
+      <RouterLink to="/" active-class="is-active" class="navbar-item">
+        Home
+      </RouterLink>
+
+      <RouterLink to="/my-activity" class="navbar-item">
+        My Activity
+      </RouterLink>
+
+      <RouterLink to="/stats" class="navbar-item">
+        Stats
+      </RouterLink>
+
+      <RouterLink to="/friends-activity" class="navbar-item">
+        Friends Activity
+      </RouterLink>
+
+      <RouterLink to="/people-search" class="navbar-item">
+        People Search
+      </RouterLink>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a to="/admin" class="navbar-link">
+          Admin
+        </a>
+
+        <div class="navbar-dropdown">
+          <RouterLink to="/users" class="navbar-item">
+            Users
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+
+</style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bulma/css/bulma.min.css'
 
 import App from './App.vue'
 import router from './router'
