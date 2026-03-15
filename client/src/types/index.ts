8ea13959a54c0668d00cfedbb90db93b0b3be4d1
Ian Kincaid <i.s.kincaid@gmail.com>
2026-03-15 12:49:12 -0400

    added User and Activity types

---
+++ client/src/types/index.ts
@@ -0,0 +1,21 @@
+export interface User {
+	userId: number;
+	profilePicture: string;
+	username: string;
+	firstName: string;
+	lastName: string;
+	isAdmin: boolean;
+}
+
+export type ActivityCategory = 'run' | 'climb' | 'bike' | 'other' | 'hike';
+
+export interface Activity {
+	userId: number;
+	date: string;
+	time: string;
+	description: string;
+	category: ActivityCategory;
+	distance: number;
+	duration: number;
+	image: string;
+}
