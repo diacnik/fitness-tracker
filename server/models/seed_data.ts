097f0e83e700d5b5b66ce539505ab5eafcc266eb
Ian Kincaid <i.s.kincaid@gmail.com>
2026-05-09 15:41:39 -0400

    updated seed_data model to seed in correct order

--- server/models/seed_data.ts
+++ server/models/seed_data.ts
@@ -1,16 +1,21 @@
 import { config } from 'dotenv';
 config();
 
-import { seed as seedUsers } from './users';
-import { seed as seedActivities } from './activities';
-import { seed as seedConnections } from './connections';
+import { seed as seedUsers } from "./users";
+import { seed as seedActivities } from "./activities";
+import { seed as seedConnections } from "./connections";
 
-Promise.all([seedUsers(), seedActivities(), seedConnections()])
-    .then(() => {
+async function runSeed() {
+    try {
+        await seedUsers();
+        await seedActivities();
+        await seedConnections();
         console.log("Seeding complete");
         process.exit(0);
-    })
-    .catch((err) => {
+    } catch (err) {
         console.error("Error seeding data:", err);
         process.exit(1);
-    });
+    }
+}
+
+runSeed();
